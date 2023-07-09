00866cb09129cdd6ee9d315b3b79426593ce5776
luoluo <601119280@qq.com>
2023-07-09 20:01:07 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,13 @@
 <script setup>
+import axios from 'axios';
+axios({
+  method: 'get',
+  url:'http://syt.atguigu.cn/api/hosp/hospital/1/5'
+}).then(response => {
+  console.log(response.data)
+}).catch(error => {
+  console.log(error)
+});
 </script>
 
 <template>
