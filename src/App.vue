4e2410c168feb584242c6c7213f60adccd6d0152
luoluo <601119280@qq.com>
2023-07-09 20:05:52 +0800

    测试2

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
-// import axios from 'axios';
-// axios({
-//   method: 'get',
-//   url:'https://syt.atguigu.cn/api/hosp/hospital/1/5'
-// }).then(response => {
-//   console.log(response.data)
-// }).catch(error => {
-//   console.log(error)
-// });
+import axios from 'axios';
+axios({
+  method: 'get',
+  url:'https://syt.atguigu.cn/api/hosp/hospital/1/5'
+}).then(response => {
+  console.log(response.data)
+}).catch(error => {
+  console.log(error)
+});
 </script>
 
 <template>
